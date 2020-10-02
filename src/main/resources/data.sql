f5f221b50aa9180e75f23369f48837027f1eb368
Antonio Soto <amsa11@alu.ua.es>
2020-10-02 14:54:53 +0200

    Data customers added.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO db.customer (id,address,cif,city,country,email,name,observations,people,phone,postal_code) VALUES
+(1,'Calle Poeta de parque nº 34 Piso 3º F','A123456789','Bacarot','España','email@email.com','La tiendita S.L.','PAGO POR ADELANTADO.','Pedro Rivera','965600123','01234'),
+(2,'Avenida La Travesía nº 11','B123456789','Alicante','España','email@email.com','El Gran Hotel S.L.','Pago a 3 meses, buen cliente.','Luis Rovira','965600123','03560'),
+(3,'C Replaceta de Mendizábal','C123456789','Segovia','España','email@email.com','Ca Paco S.A.','Nunca paga.','Fernando Di Sousa','965600123','12334');
