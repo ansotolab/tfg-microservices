5f25d0b90aef3bd04702b68fa73340306a59ec9d
Antonio Soto <amsa11@alu.ua.es>
2020-10-19 14:41:30 +0200

    Images controller tests.

--- lca-customer/src/main/java/com/lca/customer/controller/ImageController.java
+++ lca-customer/src/main/java/com/lca/customer/controller/ImageController.java
@@ -3,13 +3,19 @@ package com.lca.customer.controller;
 import com.lca.customer.model.Image;
 import com.lca.customer.service.ImageService;
 import com.lca.customer.service.ProductDetailService;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.activation.MimetypesFileTypeMap;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,9 +39,12 @@ public class ImageController {
     @GetMapping("/{id}")
     public ResponseEntity<byte[]> getFile(@PathVariable long id) {
         Image image = imageService.getFile(id);
+        MimetypesFileTypeMap fileTypeMap = new MimetypesFileTypeMap();
+        String mimeType = fileTypeMap.getContentType(image.getName());
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + image.getName() + "\"")
+                .contentType(MediaType.parseMediaType(mimeType))
                 .body(image.getData());
     }
 }
--- lca-customer/src/test/java/com/lca/customer/CustomerApplicationTests.java
+++ lca-customer/src/test/java/com/lca/customer/CustomerApplicationTests.java
@@ -2,6 +2,9 @@ package com.lca.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 //@SpringBootTest
 class CustomerApplicationTests {
@@ -9,5 +12,4 @@ class CustomerApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
