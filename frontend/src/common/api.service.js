9c55ca1be30c101625daa57cb9e24dc4b802aacd
Antonio Soto <amsa11@alu.ua.es>
2020-11-11 08:37:43 +0100

    Env Vue.

--- frontend/src/common/api.service.js
+++ frontend/src/common/api.service.js
@@ -2,14 +2,14 @@ import Vue from "vue";
 import axios from "axios";
 import VueAxios from "vue-axios";
 import JwtService from "@/common/jwt.service";
-import { API_URL } from "@/common/config";
+//import { API_URL } from "@/common/config";
 import { SEND_ALERT } from "@/store/actions.type";
 import store from "@/store";
 
 const ApiService = {
   init() {
     Vue.use(VueAxios, axios);
-    Vue.axios.defaults.baseURL = API_URL;
+    Vue.axios.defaults.baseURL = process.env.VUE_APP_BACKEND_API;
   },
 
   setHeader() {
