09bc62f91c5c2546ad4fd0fec2693cae66064a62
Antonio Soto <amsa11@alu.ua.es>
2020-11-19 09:49:06 +0100

    Updating curl method wait.sh

--- it-postman/wait.sh
+++ it-postman/wait.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-url="http://localhost:8762/api/customers"
-until curl -sf "$url"; do echo -n "."; sleep 1; done
+url="http://localhost:8762/auth/login"
+echo "Esperando a los contenedores.";
+sleep 60;
+until curl -sf --header "Content-Type: application/json" --request POST --data '{"username":"admin","password":"admin"}' "$url"; do echo -n "."; sleep 1; done
