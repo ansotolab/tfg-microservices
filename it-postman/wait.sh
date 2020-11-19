1444611ad425f1169c7d193e719e20464812a1e1
Antonio Soto <amsa11@alu.ua.es>
2020-11-19 10:44:22 +0100

    Updating wait.sh

--- it-postman/wait.sh
+++ it-postman/wait.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 url="http://localhost:8762/auth/login"
 echo "Esperando a los contenedores.";
-sleep 60;
-until curl -sf --header "Content-Type: application/json" --request POST --data '{"username":"admin","password":"admin"}' "$url"; do echo -n "."; sleep 1; done
+until curl -f --header "Content-Type: application/json" --request POST --data '{"username":"admin","password":"admin"}' "$url"; do echo -n "."; sleep 1; done
