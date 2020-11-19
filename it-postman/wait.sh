c9bf413c2bff9e5200d7370a2f7649accca01d7e
Antonio Soto <amsa11@alu.ua.es>
2020-11-19 11:37:10 +0100

    Deleting IT-Postman workflow.

--- it-postman/wait.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-url="http://localhost:8762/auth/login"
-echo "Esperando a los contenedores.";
-until curl -f --header "Content-Type: application/json" --request POST --data '{"username":"admin","password":"admin"}' "$url"; do echo -n "."; sleep 1; done
